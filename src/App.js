eef40f75ad470e13138acc8defb6acf5fdfcc9d7
agusahumada <agustinaahumada649@gmail.com>
2021-08-04 17:24:34 -0300

    New Product component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Product from './components/Product';
 
 
 function App() {
+
+  //Make 
+  const [ products, setProduct ] = useState([
+    {id: 1, name: 'Curso ReactJS', price: 100},
+    {id: 2, name: 'Curso NodeJS', price: 150},
+    {id: 3, name: 'Curso VuelJS', price: 100},
+    {id: 4, name: 'Curso Angular', price: 100},
+  ]);
+
+
 
   const fecha = new Date().getFullYear();
 
@@ -15,6 +26,13 @@ function App() {
       <Footer
         fecha = {fecha}
       />
+      {products.map(product => (
+        <Product 
+          key = {product.id}
+          product = {product}
+        />
+      ))}
+
     </Fragment>
   );
 }
